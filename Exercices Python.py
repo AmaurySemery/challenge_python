e7eb31f63abd356b2a59229e025a4c146d9937ba
Amaury Semery <amaury.semery@laposte.net>
2020-07-20 12:30:36 +0200

    Update Exercices Python.py

--- Exercices Python.py
+++ Exercices Python.py
@@ -60,3 +60,5 @@ def age():
 
 age()
 
+# Exercice 8 : Ecrire un programme en Python qui demande à l’utilisateur de saisir un nombre entier n et de lui afficher la valeur de la somme 1 + 2 + … + n =
+
