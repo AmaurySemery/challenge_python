e4ec15bc7b093d9709a3999da3f71f834fb4968a
Amaury Semery <amaury.semery@laposte.net>
2020-07-20 15:38:54 +0200

    Update Exercices Python.py

--- Exercices Python.py
+++ Exercices Python.py
@@ -171,4 +171,3 @@ else :
     
 # Exercice 16 : Ecrire un programme en langage Python qui permet de parcourir et afficher les caractères d’une variable du type chaine de caractères. Exemple pour s = « Python » , le programme affiche les caractères : P y t h o n
 
-
