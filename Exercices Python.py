521705e9a61c14a3924cd1fd5460b410446661f4
Amaury Semery <amaury.semery@laposte.net>
2020-07-20 11:44:02 +0200

    Create Exercices Python.py

---
+++ Exercices Python.py
@@ -0,0 +1,20 @@
+# Exercice 1 : Ecrire un programme en langage Python qui demande à l’utilisateur de saisir son nom et de lui afficher son nom avec un message de bienvenue !
+
+name = input("Tu t'appelles comment ? ")
+print("Bienvenue", name, "!")
+
+# Exercice 2 : Ecrire un programme en Python qui demande à l’utilisateur de saisir deux nombres a et b et de lui afficher leur somme : a + b
+
+a = input("Donne moi un premier chiffre : ")
+b = input("Donne moi un second chiffre (tu vas voir, c'est magique) : ")
+c = int(a) + int(b)
+print("Ce qui nous fait la modique somme de...", c, "!")
+
+# Exercice 3 : Ecrire un programme en Python qui demande à l’utilisateur de saisir deux nombres a et b et de lui afficher leur maximum.
+
+d = input("On va définir le maximum entre deux nombres, peux-tu me donner le premier ? ")
+e = input("Peux-tu me donner un second chiffre ? ")
+print("Le maximum est de", max(d,e), "! Félicitations", name, "!")
+
+# Exercice 4 : Ecrire un programme en langage Python qui affiche les 100 premiers nombres entiers
+
