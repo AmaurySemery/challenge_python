733044743ad47e9718dc97a2cd939101f5e36cfa
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2020-07-22 11:42:23 +0200

    Update Exercices Python 2.py

--- Exercices Python 2.py
+++ Exercices Python 2.py
@@ -4,3 +4,9 @@
 
 print("Hellow world")!
 
+# Ecrivez un extrait de code (une petite partie d'un programme Python) qui compte les têtes, les épaules, les genoux et les orteils à une fête. Le correcteur va automatiquement définir une variable personnes pour vous, elle contiendra le nombre de personnes à la fête. Votre code doit définir quatre variables, une appelée tetes, une appelée epaules, une appelée genoux et une appelée orteils, égales au nombre de têtes, épaules, genoux et orteils à la fête. Votre programme ne doit pas générer de sortie.
+
+tetes = 1 * personnes
+epaules = 2 * personnes
+genoux = 2 * personnes
+orteils = 10 * personnes
